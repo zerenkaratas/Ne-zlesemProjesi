de6f963a2c9a92412b1f09c4f74d898b88746f1b
Zeren Karataş <zerenkarats@gmail.com>
2026-01-08 21:22:21 +0300

    proje başarıyla yüklendii

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,9 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-  origin: 'http://localhost:5173',
-  credentials: true,
-});
+    origin: true, // Tüm adreslere izin verir (Sınav projesi için en hızlı çözüm)
+    credentials: true,
+  });
 
   const port = process.env.PORT ?? 3000;
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
